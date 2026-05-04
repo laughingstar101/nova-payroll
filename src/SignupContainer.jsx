3f471fa6a7c41d9dfe0ead68999c833fc160dbe3
laughingstar101 <shamhyqal@gmail.com>
2026-05-04 21:54:28 +0800

    change

--- src/SignupContainer.jsx
+++ src/SignupContainer.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 export default function SignupContainer() {
     return (
         <section id='signupContainer'>
-            <h1>Register Company</h1>    
+            <h1>Register Company!</h1>    
             <section id="formContainer">
                 <label for="nameInput" className="formLabel">Company name:</label>
                 <input type="text" id="nameInput" className="formInput"></input>
