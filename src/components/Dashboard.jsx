eef74ace7328a3beb8cf785fbc0a50111df1ec69
laughingstar101 <shamhyqal@gmail.com>
2026-05-12 19:42:19 +0800

    add signout func

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -27,6 +27,7 @@ export default function Dashboard() {
                 navigate("/");
             } else {
                 setCompany(data);
+                console.log(data)
             }
             setLoading(false);
         };
@@ -34,13 +35,25 @@ export default function Dashboard() {
     }, [navigate]);
 
     if (loading) {
-        return <div className="loader"></div>
+        return (
+            <div className="min-w-screen h-full flex justify-center items-center">
+                <div className="loader"></div>
+            </div>
+        )
     }
 
     return (
         <div>
             <TopBar/>
+            <a onClick={async () => {
+                await supabase.auth.signOut();
+                navigate('/');
+            }} className="flex items-center gap-2 text-white text-xl cursor-pointer text-center hover:underline pl-4 mt-2">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m368-417 202 202-90 89-354-354 354-354 90 89-202 202h466v126H368Z"/></svg>
+                back to register
+            </a>
             <h1>Hello world</h1>
+            <p className="text-white">{company.company_name}</p>
         </div>
     );
 }
