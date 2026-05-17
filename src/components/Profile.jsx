e637c64972ac3a193e93d2deff85325305c7a3ce
laughingstar101 <shamhyqal@gmail.com>
2026-05-18 03:19:55 +0800

    profile page fetch

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from 'react'
 import profileImg from '../assets/profile-empty.png'
 import TopBar from './TopBar'
+import { supabase } from '../utils/supabase/supabase'
+import { useNavigate } from 'react-router-dom'
 
 export default function Profile() {
+    const navigate = useNavigate();
+    const [employee, setEmployee] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data: { user } } = await supabase.auth.getUser();
+            if (!user) {
+                alert("User not found. Redirecting..");
+                navigate("/");
+                return;
+            }
+            try {
+                const { data, error } = await supabase
+                    .from('Employee')
+                    .select('employee_name')
+                    .eq('employee_email', user.email)
+                    .single();
+
+                if (error) throw error;
+                setEmployee(data);
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+                setEmployee(null);
+                alert("Error fetching data from database. Please try again later.")
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [navigate])
+
+    if (loading) {
+        return (
+            <div className='min-w-screen h-full flex justify-center items-center'>
+                <div className="loader"></div>
+            </div>
+        )
+    }
+
     return (
         <div className="min-h-screen flex flex-col bg-linear-to-br from-secondary-colour3 to-secondary-colour2">
             <TopBar/>
