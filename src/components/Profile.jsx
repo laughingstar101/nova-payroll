072bc75145c97eee80186175b1be46ed256af4f4
laughingstar101 <shamhyqal@gmail.com>
2026-05-18 03:55:21 +0800

    finish profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -41,6 +41,10 @@ export default function Profile() {
         navigate('/dashboard');
     }
 
+    const handleGoToResetPassword = () => {
+        navigate('/update-password')
+    }
+
     if (loading) {
         return (
             <div className='min-w-screen h-full flex justify-center items-center bg-linear-to-br from-secondary-colour3 to-secondary-colour2'>
@@ -54,9 +58,11 @@ export default function Profile() {
             <TopBar/>
             <div className='flex flex-col items-center m-auto w-full'>
                 <div className="container m-auto flex flex-col gap-4 items-center py-8 rounded-xl bg-primary-colour shadow-xl">
-                    <p className='text-6xl mb-12 text-white font-hero!'>{employee.employee_name}</p>
+                    <p className='text-6xl mb-12 text-white font-hero!'>Profile</p>
                     <img className='w-40' src={profileImg}></img>
-                    <p>{employee.employee_email}</p>
+                    <p className='text-xl text-white'>Name: {employee.employee_name}</p>
+                    <p className='text-white text-xl'>Email: {employee.employee_email}</p>
+                    <button className='text-black text-lg bg-complementary-colour2 px-4 py-2 mt-4 rounded-lg hover:scale-110 transition-transform hover:shadow-md hover:shadow-black hover:cursor-pointer' onClick={handleGoToResetPassword}>Reset Password</button>
                     <p className='text-white mt-8 hover:underline hover:cursor-pointer' onClick={handleGoToDashboard}>Back to dashboard</p>
                 </div>
             </div>
