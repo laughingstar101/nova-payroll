7b910538f1a7957a936304ab9f5fab3a9812f9bf
laughingstar101 <shamhyqal@gmail.com>
2026-05-17 20:22:05 +0800

    changed font

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,7 @@ export default function Profile() {
         <div className="min-h-screen flex flex-col bg-linear-to-br from-secondary-colour3 to-secondary-colour2">
             <div className="container m-auto flex flex-col gap-4 items-center py-8 rounded-xl bg-primary-colour shadow-xl">
                 <img className='w-40' src={profileImg}></img>
-                <p className='text-4xl text-white'>Username</p>
+                <p className='text-4xl text-white font-hero!'>Username</p>
             </div>
         </div>
     )
