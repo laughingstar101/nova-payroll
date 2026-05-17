a5c67b03c2b54415379a7338e832bc96be41a234
laughingstar101 <shamhyqal@gmail.com>
2026-05-17 20:18:44 +0800

    profile container

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,12 @@
+import profileImg from '../assets/profile-empty.png'
+
 export default function Profile() {
     return (
-        <></>
+        <div className="min-h-screen flex flex-col bg-linear-to-br from-secondary-colour3 to-secondary-colour2">
+            <div className="container m-auto flex flex-col gap-4 items-center py-8 rounded-xl bg-primary-colour shadow-xl">
+                <img className='w-40' src={profileImg}></img>
+                <p className='text-4xl text-white'>Username</p>
+            </div>
+        </div>
     )
 }
