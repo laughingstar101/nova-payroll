94d939470c74ee293286942a98dea445d18208ee
laughingstar101 <shamhyqal@gmail.com>
2026-05-18 03:06:54 +0800

    add topbar to profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,10 @@
 import profileImg from '../assets/profile-empty.png'
+import TopBar from './TopBar'
 
 export default function Profile() {
     return (
         <div className="min-h-screen flex flex-col bg-linear-to-br from-secondary-colour3 to-secondary-colour2">
+            <TopBar/>
             <div className="container m-auto flex flex-col gap-4 items-center py-8 rounded-xl bg-primary-colour shadow-xl">
                 <img className='w-40' src={profileImg}></img>
                 <p className='text-4xl text-white font-hero!'>Username</p>
