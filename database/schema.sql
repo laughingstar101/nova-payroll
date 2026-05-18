983aace5810b5e42d1ae1f966479c92da417381a
laughingstar101 <shamhyqal@gmail.com>
2026-05-18 14:32:36 +0800

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -5,15 +5,15 @@
 create table if not exists public."Company" (
   id uuid not null default gen_random_uuid (),
   company_email text not null,
-  company_name text null,
+  company_name text not null,
   company_id uuid not null,
   created_at timestamp with time zone null default now(),
   constraint Company_pkey primary key (id),
   constraint Company_company_id_fkey foreign KEY (company_id) references auth.users (id)
-) TABLESPACE pg_default;
+);
 
 -- Employee table
-create table if not public."Employee" (
+create table if not exists public."Employee" (
   id uuid not null default gen_random_uuid (),
   employee_email text not null,
   employee_name text not null,
@@ -22,4 +22,4 @@ create table if not public."Employee" (
   created_at timestamp with time zone null default now(),
   constraint Employee_pkey primary key (id),
   constraint Employee_employee_company_fkey foreign KEY (employee_company) references "Company" (id)
-) TABLESPACE pg_default;
+);
