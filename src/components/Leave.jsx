d1c17d7d573224fa21eff8ac1ccf29d8687ba078
laughingstar101 <shamhyqal@gmail.com>
2026-05-25 03:45:04 +0800

    uses arrow function

--- src/components/Leave.jsx
+++ src/components/Leave.jsx
@@ -76,8 +76,9 @@ export default function Leave() {
                     back
                 </a>
                 <img src={logoImg} className="h-15 justify-self-center" height='30'></img>
-                <img src={profileImg} className="h-15 hover:cursor-pointer justify-self-end"></img>
+                <img onClick={() => navigate("/profile")} src={profileImg} className="h-15 hover:cursor-pointer justify-self-end"></img>
             </div>
+            <p className='text-5xl mb-12 text-white font-hero! text-center'>Leave Application</p>
             <div className="container bg-primary-colour mx-auto flex flex-col items-center mt-12 px-12 py-8 rounded-md shadow-xl">
                 {employee == null || !employee && (
                     <p>Employee doesn't exist</p>
