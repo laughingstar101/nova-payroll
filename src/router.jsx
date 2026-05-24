285caf1521e0569510491a18e4f1d4ddf8930dd8
laughingstar101 <shamhyqal@gmail.com>
2026-05-25 02:52:49 +0800

    add leave page router

--- src/router.jsx
+++ src/router.jsx
@@ -5,11 +5,13 @@ import ProtectedRoute from "./ProtectedRoute";
 import UpdatePassword from "./components/UpdatePassword";
 import CompanyRegister from "./components/CompanyRegister";
 import Profile from "./components/Profile";
+import Leave from "./components/Leave"
 
 export const router = createBrowserRouter([
     {path: "/", element: <App/>},
     {path: "/dashboard", element: <ProtectedRoute><Dashboard/></ProtectedRoute>},
-    {path: "update-password", element: <UpdatePassword/>},
-    {path: "company-register", element: <CompanyRegister/>},
-    {path: "profile", element: <Profile/>},
+    {path: "/update-password", element: <UpdatePassword/>},
+    {path: "/company-register", element: <CompanyRegister/>},
+    {path: "/profile", element: <Profile/>},
+    {path: "/leave", element: <Leave/>},
 ])  
