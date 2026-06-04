4619226535e528135ffcf435b3f34ba6d4c7d5db
laughingstar101 <shamhyqal@gmail.com>
2026-06-04 18:30:25 +0800

    remove loading

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -4,16 +4,12 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
     const [session, setSession] = useState(undefined);
-    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         supabase.auth.getSession().then(({ data: { session }}) => {
             setSession(session);
-            setLoading(false);
         });
     }, []);
-
-    if (loading) return <div>Loading..</div>
 
     return session ? children : <Navigate to="/" />
 }
